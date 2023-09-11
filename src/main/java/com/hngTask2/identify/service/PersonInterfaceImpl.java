b21c3dbd9dfdaf058db7b0ef728e00d29e1a6a74
chinwoke <canugwara99@gmail.com>
2023-09-11 14:39:24 +0100

    business logic done

--- src/main/java/com/hngTask2/identify/service/PersonInterfaceImpl.java
+++ src/main/java/com/hngTask2/identify/service/PersonInterfaceImpl.java
@@ -1,12 +1,16 @@
 package com.hngTask2.identify.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 import com.hngTask2.identify.data.dto.request.PersonRequest;
 import com.hngTask2.identify.data.dto.response.ApiResponse;
 import com.hngTask2.identify.data.model.Person;
 import com.hngTask2.identify.data.repository.PersonRepository;
 import com.hngTask2.identify.exception.BusinessLogicException;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -18,6 +22,7 @@ import static com.hngTask2.identify.utility.IdentifyUtilities.MAX_NUMBER_PER_PAG
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class PersonInterfaceImpl implements PersonInterface{
     private final PersonRepository personRepository;
     @Override
@@ -61,11 +66,28 @@ public class PersonInterfaceImpl implements PersonInterface{
 
     @Override
     public Person updatePerson(Long personId, JsonPatch updatePayload) {
-        return null;
+        ObjectMapper mapper = new ObjectMapper();
+        Person foundPerson = getPersonById(personId);
+        //parse person object to node
+        JsonNode node = mapper.convertValue(foundPerson, JsonNode.class);
+        try {
+            //apply patch
+            JsonNode updatedNode = updatePayload.apply(node);
+            //parse node back to passenger object
+            Person updatedPerson = mapper.convertValue(updatedNode, Person.class);
+            updatedPerson = personRepository.save(updatedPerson);
+            return updatedPerson;
+        } catch (JsonPatchException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException();
+        }
+
+
     }
 
     @Override
     public void deletePerson(Long personId) {
+        personRepository.deleteById(personId);
 
     }
 }
