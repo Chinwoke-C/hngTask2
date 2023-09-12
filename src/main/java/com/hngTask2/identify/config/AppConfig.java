19b25d8272e4ab9bef2cd15c9a819bc2e3922a54
chinwoke <canugwara99@gmail.com>
2023-09-12 13:46:24 +0100

    update TBD

--- src/main/java/com/hngTask2/identify/config/AppConfig.java
+++ src/main/java/com/hngTask2/identify/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.hngTask2.identify.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,7 @@ public class AppConfig {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper()
+                .registerModule(new JavaTimeModule())
                 .findAndRegisterModules();
     }
 }
